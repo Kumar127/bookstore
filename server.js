c1773a1b208d58fb71d77576f6ad4853da19777f
Kumar127 <abhishekdude612@gmail.com>
2020-07-16 16:00:13 +0530

    initial setup

---
+++ server.js
@@ -0,0 +1,27 @@
+if(process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+const express =  require('express');
+const app = express();
+const expressLayouts = require('express-ejs-layouts')
+
+ const indexRouter = require('./routes/index');
+
+app.set('view engine', 'ejs'); // set the view engine i.e EJS in our case
+app.set('views', __dirname + '/views'); //set view to the cuurent directory views folder
+app.set('layout', 'layouts/layout'); // set the layout to the particular folder
+app.use(expressLayouts) // to keep the basic layout in html for e.g Header,footer
+app.use(express.static('public')) // Keeps the style sheet file & javascripts file
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+const db = mongoose.connection;
+db.on('error',error => console.error(error));
+db.once('open', () => console.log('connected to mongoose'))
+
+app.use('/',indexRouter)
+
+app.listen(process.env.PORT || 3000,() => console.log(`server running at the port 3000`))
